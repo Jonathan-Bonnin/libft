089bf5c30c4476f2cb3294b2978b3a895c358330
Jonathan Bonnin <jbonnin@c3r7s2.42prague.com>
2023-01-15 21:49:52 +0100

    add const to ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,21 +6,25 @@
 /*   By: jbonnin <jbonnin@42student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 18:50:06 by jbonnin           #+#    #+#             */
-/*   Updated: 2023/01/15 16:54:57 by jbonnin          ###   ########.fr       */
+/*   Updated: 2023/01/15 21:15:05 by jbonnin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strrchr(char *s, int c)
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	int		i;
+	char	*copy;
 
 	i = 0;
 	while (s[i])
 		i++;
+	copy = (char *) s;
 	if (c == 0)
-		return (&s[i]);
+		return (&copy[i]);
 	while (--i >= 0)
 		if (s[i] == c)
-			return (&s[i]);
+			return (&copy[i]);
 	return (0);
 }
