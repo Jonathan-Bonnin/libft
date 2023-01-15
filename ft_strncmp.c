af77c4f523d2c11554f37b2bb19b67904009c26d
Jonathan Bonnin <jbonnin@c1r7s1.42prague.com>
2023-01-15 16:44:15 +0100

    fix ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jbonnin <jbonnin@42student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 13:28:05 by jbonnin           #+#    #+#             */
-/*   Updated: 2023/01/09 19:06:55 by jbonnin          ###   ########.fr       */
+/*   Updated: 2023/01/15 16:41:27 by jbonnin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
-	if (!n)
+	if (n == 0)
 		return (n);
 	i = 0;
 	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 }
