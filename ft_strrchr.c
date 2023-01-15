fb1774e150817cd9ff2028534924752bc1767062
Jonathan Bonnin <jbonnin@c1r7s1.42prague.com>
2023-01-15 16:55:23 +0100

    fix ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,28 +6,21 @@
 /*   By: jbonnin <jbonnin@42student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 18:50:06 by jbonnin           #+#    #+#             */
-/*   Updated: 2023/01/14 20:13:51 by jbonnin          ###   ########.fr       */
+/*   Updated: 2023/01/15 16:54:57 by jbonnin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strrchr(char *s, int c)
 {
 	int	i;
-	int	is_c_in_s;
 
 	i = 0;
-	is_c_in_s = 0;
 	while (s[i])
 		i++;
 	if (c == 0)
 		return (&s[i]);
-	if (!is_c_in_s)
-		return (0);
-	while (i >= 0)
-	{
+	while (--i >= 0)
 		if (s[i] == c)
-			break ;
-		i--;
-	}
-	return (&s[i]);
+			return (&s[i]);
+	return (0);
 }
