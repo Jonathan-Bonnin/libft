c5625d60e710589e26dea13a1340a0ca94a2388e
Jonathan Bonnin <jbonnin@c1r4s3.42prague.com>
2023-01-17 21:31:39 +0100

    fix ft_substr.c to pass every case

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jbonnin <jbonnin@42student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 19:45:49 by jbonnin           #+#    #+#             */
-/*   Updated: 2023/01/17 19:34:44 by jbonnin          ###   ########.fr       */
+/*   Updated: 2023/01/17 21:30:27 by jbonnin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,18 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*res;
-	int		i;
-	int		s_len;
+	size_t	i;
+	size_t	s_len;
 
 	if (!s)
 		return (0);
 	res = malloc(len + 1);
 	s_len = ft_strlen(s);
-	if (!res || start >= (unsigned int) s_len)
+	if (!res)
 		return (0);
 	i = 0;
-	while ((int) start < s_len && i < (int) len)
+	while (start < (unsigned int) s_len && i < len)
 		res[i++] = s[start++];
 	res[i] = 0;
 	return (res);
-
 }
