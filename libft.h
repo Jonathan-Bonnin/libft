f25d1484189b1170b6988656362b15de70f5d58e
Jonathan Bonnin <jbonnin@c3r7s4.42prague.com>
2023-01-21 20:03:30 +0100

    add ft_itoa.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jbonnin <jbonnin@42student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 20:02:59 by jbonnin           #+#    #+#             */
-/*   Updated: 2023/01/21 18:00:58 by jbonnin          ###   ########.fr       */
+/*   Updated: 2023/01/21 18:50:03 by jbonnin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
